bb65b8f2484771b7c29fb22c33505ad20a3c7319
okriszi999 <72317895+okriszi999@users.noreply.github.com>
2021-05-13 16:50:30 +0200

    Color input remove

--- src/component/configurator.js
+++ src/component/configurator.js
@@ -14,8 +14,7 @@ function Configurator({getOriginalBlur, getOriginalTransparency}) {
             <h1>Configurator</h1>
            <RangeInput min="0" max="100" step="5" value={transparency} setValue={setTransparency} title="Transparency" />
            <RangeInput min="0" max="2000" step="100" value={blur} setValue={setBlur} title="Blur Value" />
-           <input type="color"/>
-           Css
+           
             <code>
                 <span><span class="property">background</span>: rgba<span class="bracket">(</span><span class="value">255</span>,<span class="value">255</span>,<span class="value">255</span>, <span class="value">{transparency / 100}</span><span class="bracket">)</span>;</span>
                 <span><span class="property">backdrop-filter</span>: blur<span class="bracket">(</span><span class="value">{blur / 100}px</span><span class="bracket">)</span>;</span>
